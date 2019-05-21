61508cba8539bf4d7debbe8de80b9e4afc1cfc20
UL7RA <hofnar13@gmail.com>
2019-05-21 14:05:25 +0300

    Added presentation notification

--- app/src/main/java/com/buttons/forgetmenot/PlantAddScreen.java
+++ app/src/main/java/com/buttons/forgetmenot/PlantAddScreen.java
@@ -1,6 +1,8 @@
 package com.buttons.forgetmenot;
 
 import android.app.DatePickerDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -8,8 +10,11 @@ import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
 import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Debug;
 import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageButton;
@@ -253,6 +258,22 @@ public class PlantAddScreen extends AppCompatActivity {
                             toast.show();
                             db.close();
                             setResult(RESULT_OK);
+                            //fire presentation notitfication
+
+                            createNotificationChannel();
+
+                            NotificationCompat.Builder builder = new NotificationCompat.Builder(PlantAddScreen.this, CHANNEL_ID)
+                                    .setSmallIcon(R.drawable.water)
+                                    .setContentTitle(getString(R.string.notificationTitleWater))
+                                    .setContentText(getString(R.string.notificationTextWater) + " " + name + "!")
+                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(PlantAddScreen.this);
+
+                        // notificationId is a unique int for each notification that you must define
+                            notificationManager.notify(0, builder.build());
+
+
                             finish();
                         }
                     }
@@ -341,4 +362,23 @@ public class PlantAddScreen extends AppCompatActivity {
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dipValue, metrics);
     }
+
+    //presentation notification
+    private static final String CHANNEL_ID = "ForgetMeNot";
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
